df33e9ae4b01275588e28122d86f47b4448d11f1
Mehedi Alam <mehedialam109@gmail.com>
2022-01-02 12:45:51 +0600

    first commit

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Banner.css";
+import banner from "../../../images/img-8.jpg";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <img src={banner} alt="" />
+    </div>
+  );
+};
+
+export default Banner;
