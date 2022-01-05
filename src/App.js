cba26cea123fbd1c157ef9b29369682477cd59e7
xaveerarif <xaveerarif@gmail.com>
2022-01-05 20:30:25 +0600

    display all rent

--- src/App.js
+++ src/App.js
@@ -7,14 +7,18 @@ import Dashboard from "./Pages/Dashboard/Dashboard/Dashboard";
 import About from "./Pages/About/About";
 import Blog from "./Pages/Blog/Blog";
 import AddHouse from "./Pages/Dashboard/AddHouse/AddHouse";
+import Rent from "./Pages/Rent/Rent";
+import Header from "./Pages/Shared/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header></Header>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/rent" element={<Rent />} />
           <Route path="/about" element={<About />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/dashboard" element={<Dashboard />}>
